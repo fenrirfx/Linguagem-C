e64837eaf04809212aad7d5c111be3ac0c99de5b
fenrirfx <83608537+fenrirfx@users.noreply.github.com>
2021-06-11 00:03:35 +0100

    Update card.c

--- card.c
+++ card.c
@@ -1,6 +1,22 @@
 # include <stdio.h>
 # include <string.h>
 # include <stdlib.h>
+# include <ctype.h>
+
+char * espacos( char * cartao, const char * aux )
+{
+    char * p = aux;
+    int i = 0;
+
+    while( *p )
+    {
+        if( !isspace(*p) )
+            cartao[i++] = *p;
+        p++;
+    }
+    return cartao;
+}
+
 
 int print(char *x, int size){
 
@@ -10,24 +26,18 @@ int print(char *x, int size){
         else
             printf("%c",x[i]);
     }
-
 }
-
 int main(){
 
-    char cartao[20];
-    int tam;
-
+    char cartao[50],aux[50];
+    int tam,val;
 
     printf("insira um numero: ");
-    scanf("%s",cartao);
-    tam=strlen(cartao);
+    fgets(aux,sizeof(aux),stdin);
+    espacos(cartao,aux);
+    tam = strlen(cartao);
 
+    for(int i=0;i<tam;i++)
+        printf("%c",cartao[i]);
     print(cartao,tam);
-
-
-
-
 return 0;
-}
-
